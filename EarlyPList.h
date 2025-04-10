a177762cd7a21577f77dbb248ad97e6bc0750918
nancyashraff <nancy.sabry05@eng-st.cu.edu.eg>
2025-04-10 20:57:54 +0200

    Update EarlyPList.h

--- EarlyPList.h
+++ EarlyPList.h
@@ -5,7 +5,12 @@
 
 class EarlyPList : public PriQueue<Patient*> 
 {
+    int Presc; // Rescheduling probability (0-100)
 public:
+    EarlyPList(int rescheduleProb) : Presc(rescheduleProb) {}
+
     Patient* reschedule();
 };
+
+
 
