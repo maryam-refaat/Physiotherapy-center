d50d9e89e2059cd5db7754de17b1d6128250e397
nancyashraff <nancy.sabry05@eng-st.cu.edu.eg>
2025-04-12 13:16:11 +0200

    added static member

--- Resource.cpp
+++ Resource.cpp
@@ -1,8 +1,12 @@
 #include "Resource.h"
 
-Resource::Resource(TreatmentType t,int c=1) :  type(t), id(next_id++), capacity(c)
+int Resource::next_id = 1;
+
+Resource::Resource(TreatmentType t,int c) :  type(t), id(next_id++), capacity(c)
 {
 }
-int Resource::get_id(){
+
+int Resource::get_id()
+{
     return id;
 }
